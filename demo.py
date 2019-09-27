4efcf0533f9f54057526d2f883233409fec73c57
Elijah Underhill-Miller <shnibble@Shnibbers-Bimble.local>
2019-09-26 23:30:37 -0500

    Finished week 3 challange

--- demo.py
+++ demo.py
@@ -2,14 +2,28 @@
 
 import turtle
 
+x = int(input("Enter #:"))
+i = 0
 turtle.color("green", "purple")
 turtle.begin_fill()
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
+while i < x:
+    turtle.forward(50)
+    turtle.left(360/x)
+    i = i+1
 turtle.end_fill()
 turtle.done()
+
+
+#  i=0
+#  while True:
+#      turtle.forward(50)
+#      turtle.left(360/x)
+#      turtle.end_fill()
+#      i + 1
+#      if i == x:
+#          break
+#      turtle.done()
+#      turtle.forward(50)
+#      turtle.left(360/x)
+    
+    
