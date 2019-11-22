945f912628525a6618465f9c3ecc8e5e855b718b
Elijah Underhill-Miller <shnibble@Shnibbers-Bimble.local>
2019-11-21 20:24:52 -0600

    now searches for a replaces all bad words!

--- word_checker.py
+++ word_checker.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python3
 
 import sys
-# here are some really bad word like
 
 bad_words = ["like", "very", "really"]
 
 print("Welcome to Word Checker 1.0!")
-raw_input = str(input("Enter Words Here: "))
-raw_input.lower()
+num_bad_words = len(bad_words)
+pre_raw_input = str(input("Enter Words Here: "))
+raw_input = pre_raw_input.lower()
 def fix(user_input):
     user_input.strip()
     user_input.lower()
-    print("stating char position = " + str(user_input.find("like")))
-    x = user_input.find("like")
-    delta_char = x
-    while user_input[delta_char].isspace() != True:
-        delta_char += 1
-    fixed_input = user_input.replace(user_input[x:delta_char + 1], "")
+    #print("stating char position = " + str(user_input.find("like")))
+    count = 0
+    for rounds in range(3): # Counts how many bad words there are
+        #print("Round: " + str(rounds))
+        start_char = 0
+        while True:
+            a = user_input.find(bad_words[rounds],start_char)
+            if a == -1: # (if there no more)
+                break
+            count+=1
+            start_char=a+1
+        rounds+=1
+    for rounds in range(count):
+        if "like" in user_input:
+            x = user_input.find("like")
+        elif "very" in user_input:
+            x = user_input.find("very")
+        elif "really" in user_input:
+            x = user_input.find("really")
+        delta_char = x
+        while user_input[delta_char].isspace() != True:
+            delta_char += 1
+        fixed_input = user_input.replace(user_input[x:delta_char + 1], "")
     print(fixed_input)
     
 def ask():
     val = False
-    if bad_words[0] in raw_input:
-        val = True
-    if bad_words[1] in raw_input:
-        val = True
-    if bad_words[2] in raw_input:
-        val = True
+    for x in range(3):
+        if bad_words[x] in raw_input:
+            val = True
     if val == True:
         print("Found some bad words!")
         check_input = str(input("Fix words?[y/n]"))
