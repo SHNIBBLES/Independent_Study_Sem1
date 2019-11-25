6134322a58910c5eaaff948d45808d5c4406d1dc
Elijah Underhill-Miller <shnibble@Shnibbers-Bimble.local>
2019-11-24 18:53:33 -0600

    Updated Word Checker- bug when multiple bad words occur fixed and code as dry as some aged beef jerky

--- word_checker.py
+++ word_checker.py
@@ -5,47 +5,33 @@ import sys
 bad_words = ["like", "very", "really"]
 
 print("Welcome to Word Checker 1.0!")
-num_bad_words = len(bad_words)
 pre_raw_input = str(input("Enter Words Here: "))
-raw_input = pre_raw_input.lower()
+raw_input = pre_raw_input.lower().strip()
 def fix(user_input):
-    user_input.strip()
-    user_input.lower()
-    #print("stating char position = " + str(user_input.find("like")))
-    count = 0
-    for rounds in range(len(bad_words)): # Counts how many bad words there are
-        #print("Round: " + str(rounds))
-        start_char = 0
+    for word in bad_words:
         while True:
-            a = user_input.find(bad_words[rounds],start_char)
-            if a == -1: # (if there no more)
+            start_point = user_input.find(word)
+            if start_point == -1: # (if there no more)
                 break
-            count+=1
-            start_char=a+1
-        rounds+=1
-    for rounds in range(count):
-        if "like" in user_input:
-            x = user_input.find("like")
-        elif "very" in user_input:
-            x = user_input.find("very")
-        elif "really" in user_input:
-            x = user_input.find("really")
-        delta_char = x
-        while user_input[delta_char].isspace() != True:
-            delta_char += 1
-        fixed_input = user_input.replace(user_input[x:delta_char + 1], "")
-    print(fixed_input)
+            delta_char = start_point
+            print("delta_char = " + str(delta_char))
+            print("len(user_input) = " + str(len(user_input)))            
+            while user_input[delta_char].isspace() != True:
+                delta_char += 1
+                if delta_char >= len(user_input):
+                    break
+            next_start = min(delta_char + 1, len(user_input))
+            user_input = user_input.replace(user_input[start_point:next_start], "")
+    print(user_input)
     
 def ask():
-    val = False
+    has_bad = False
     for x in range(len(bad_words)):
         if bad_words[x] in raw_input:
-            val = True
-    if val == True:
+            has_bad = True
+    if has_bad:
         print("Found some bad words!")
-        check_input = str(input("Fix words?[y/n]"))
-        check_input.strip()
-        check_input.lower()
+        check_input = str(input("Fix words?[y/n]")).strip().lower()
         if "y" in check_input:
             fix(raw_input) 
         elif "n" in check_input:
