6113442d1a6fba13693c19bc2aba962b00c9e9b0
Elijah Underhill-Miller <shnibble@Shnibbers-Bimble.local>
2020-04-19 15:09:15 -0500

    Prints "hello world" in bottom left of window

--- eliGame.py
+++ eliGame.py
@@ -2,4 +2,18 @@
 
 
 import cocos
-print("Hello World")
+class HelloWorld(cocos.layer.Layer):
+    def __init__(self):
+        super(HelloWorld, self).__init__()
+        label = cocos.text.Label(
+            "Hello, World",
+            font_name = "Times New Roman",
+            font_size = 32,
+            anchor_x="left", anchor_y="bottom"
+        )
+        label.position = 0, 0
+        self.add(label)
+cocos.director.director.init()
+text_layer = HelloWorld()
+main_scene = cocos.scene.Scene (text_layer)
+cocos.director.director.run(main_scene)
