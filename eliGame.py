e3a1249cb39ccaa16a0e1bcc65999183dd895964
Elijah Underhill-Miller <shnibble@Shnibbers-Bimble.local>
2020-05-04 15:01:48 -0500

    classed it!

--- eliGame.py
+++ eliGame.py
@@ -5,37 +5,44 @@ import time
 import sys
 pygame.init()
 
+class Ball():
+    def __init__(self, image_file, screen):
+        self.speed = [2, 2]
+        self.rotation = 0
+        self.orgigball = pygame.image.load(image_file)
+        self.orgigball.convert()
+        self.ball = self.orgigball
+        self.ballrect = self.ball.get_rect()
+        self.radius = (self.ballrect.width / 2)
+        self.screen = screen
+    def step(self):
+        self.rotate()
+        self.bounce()
+        self.screen.blit(self.ball, (self.ballrect.centerx - int(self.ball.get_width() / 2), self.ballrect.centery - int(self.ball.get_height() / 2)))
+    def rotate(self):
+        self.rotation = (self.rotation + 6) % 360
+        self.ball = pygame.transform.rotate(self.orgigball, self.rotation)
+    def bounce(self):
+        self.ballrect = self.ballrect.move(self.speed)
+        if (self.ballrect.centerx <= 0 + self.radius) or (self.ballrect.centerx >= self.screen.get_width() - self.radius):
+            self.speed[0] = -self.speed[0]
+        if (self.ballrect.centery <= 0 + self.radius) or (self.ballrect.centery >= self.screen.get_height() - self.radius):
+            self.speed[1] = -self.speed[1]
+
+
 size = width, height = 320, 240
-speed = [2, 2]
 black = 0, 0, 0
-green = 240, 40, 70
-center = width // 2, height // 2
-rotation = 0
+green = 0, 240, 40
+
 screen = pygame.display.set_mode(size)
 
-orgigball = pygame.image.load("intro_ball.gif")
-orgigball.convert()
-ball = orgigball
-ballrect = ball.get_rect()
-ballrect.center = center
-hitDis = (ballrect.width / 2)
+ball_one = Ball("intro_ball.gif", screen)
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-    rotation = (rotation + 6) % 360
-    ball = pygame.transform.rotate(orgigball, rotation)
-    
-    ballrect = ballrect.move(speed)
-    rectCenterX = ballrect.centerx
-    rectCenterY = ballrect.centery
-    if (rectCenterX <= 0 + hitDis) or (rectCenterX >= width - hitDis):
-        speed[0] = -speed[0]
-    if (rectCenterY <= 0 + hitDis) or (rectCenterY >= height - hitDis):
-        speed[1] = -speed[1]
     screen.fill(black)
-    pygame.draw.rect(screen, green, ballrect, 1)
-    screen.blit(ball, (ballrect.centerx - int(ball.get_width() / 2), ballrect.centery - int(ball.get_height() / 2)))
-    time.sleep(.05)
+    ball_one.step()
     pygame.display.flip()
-
+    time.sleep(.05)
